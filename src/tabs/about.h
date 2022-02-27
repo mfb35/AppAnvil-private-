c81cd7faf81538302f203b70738dd6a3e11af958
nad104 <70152220+nad104@users.noreply.github.com>
2022-02-27 14:58:33 -0500

    Add files via upload

--- src/tabs/about.h
+++ src/tabs/about.h
@@ -3,11 +3,12 @@
 
 #include "status.h"
 
+#include <gtkmm/fixed.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/comboboxtext.h>
 #include <gtkmm/enums.h>
-#include <gtkmm/filechooser.h>
 #include <gtkmm/filechooserdialog.h>
+#include <gtkmm/filechooser.h>
 #include <gtkmm/grid.h>
 #include <gtkmm/liststore.h>
 #include <gtkmm/notebook.h>
@@ -18,27 +19,31 @@
 #include <gtkmm/treeview.h>
 #include <gtkmm/treeviewcolumn.h>
 #include <memory>
-#include <regex>
 #include <string>
 #include <vector>
 
 class About : public Gtk::ScrolledWindow
 {
-public:
-  About();
+  public:
+    About();
+    
+  protected:
+    // Signal handler
+    void on_fc_button_clicked();
+    void handle_file_chooser_dialogue(int response_id);
+    void on_confirm_clicked();
 
-private:
-  // GUI Builder to parse UI from xml file
-  Glib::RefPtr<Gtk::Builder> builder;
+  private:
+    // GUI Builder to parse UI from xml file
+    Glib::RefPtr<Gtk::Builder> builder;
 
-  //container Widgets
-  std::unique_ptr<Gtk::Box> a_box;
-  std::unique_ptr<Gtk::Label> a_label;
+    // Widgets on the tab   
+    std::unique_ptr<Gtk::Box> a_box;
+    //std::unique_ptr<Gtk::Fixed> a_fixed;
+    std::unique_ptr<Gtk::Label> a_label;
 
-  const std::vector<std::string> col_names{"Profile", "Status"};
-  std::shared_ptr<StatusColumnRecord> col_record;
-
-  template<typename T_Widget> static std::unique_ptr<T_Widget> get_widget(Glib::ustring name, const Glib::RefPtr<Gtk::Builder> &builder);
+    template <typename T_Widget>
+    static std::unique_ptr<T_Widget> get_widget(Glib::ustring name, const Glib::RefPtr<Gtk::Builder>& builder);
 };
 
 #endif // TABS_ABOUT_H
